3bbcb87408485207080357519bcfd038dcbf40a9
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2023-04-03 13:08:47 -0300

    tirei o husky

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,6 +5,7 @@ export const theme = {
     secondary: '#dc143c',
     white: '#FFFFFF',
     mediumGray: '#DDDDDD',
+    darkerGray: '#AAAAAA',
   },
   font: {
     family: {
@@ -34,5 +35,9 @@ export const theme = {
     xxlarge: '4.8rem',
     huge: '5.6rem',
     xhuge: '6.4rem',
+  },
+  sizes: {
+    max: '96rem',
+    content: '80rem',
   },
 } as const;
